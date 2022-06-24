a3c0444d7ec3fa3fc995b353ecb56b8f956bbe70
Kardast <dottorsf@gmail.com>
2022-06-24 16:57:12 +0200

    Censored word

--- README.md
+++ README.md
@@ -13,5 +13,7 @@ Stampare a schermo il paragrafo e la sua lunghezza.
 <!-- ok -->
 
 Una parola da censurare viene passata dall'utente tramite parametro GET.
+<!-- ok -->
 
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+<!-- ok -->
--- badwords.php
+++ badwords.php
@@ -1,7 +1,12 @@
 <?php 
 // Defining my text and variables
-$myText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia magnam quaerat, dolor ullam, dignissimos tempore doloremque error laboriosam est cupiditate, voluptatem illo corrupti! Reprehenderit quidem inventore esse veritatis sit quos! ';
-$myName = 'Sandro';
+$myText = 'I tried so hard and got so far
+But in the end, it doesn’t even matter
+I had to fall to lose it all
+But in the end, it doesn’t even matter. ';
+$myName = 'Quack';
+$badWord = $_GET['word'];
+$myReplacedText = str_replace('end', $badWord, $myText);
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +20,16 @@ $myName = 'Sandro';
 <body>
     
     <h1>Atterraggio su PHP</h1>
+
+    <!-- Initial text -->
     <p>Il mio testo è: <?php echo $myText . $myName?></p>
     <h2>La lunghezza del mio testo è: <?php echo strlen($myText)?></h2>
+
+    <!-- Bad word -->
+    <h2>La bad word è: <?php echo $badWord ?></h2>
+
+    <!-- Reprint -->
+    <p>Il mio testo censurato è: <?php echo $myReplacedText . $myName?></p>
+    <h2>La lunghezza del mio testo è: <?php echo strlen($myReplacedText)?></h2>
 </body>
 </html>
